038bc1955b5a330e97c4e8a86d7db4c6961c2cd8
Ouch Liu <ouch.liu@xuenn.com>
2018-01-08 15:42:56 +0800

    Commit code

---
+++ XamlStandardPreview/XamlStandardPreview.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+//加入 Xamarin.Forms.Alias 的參考                        
+using Xamarin.Forms.Alias;
+
+namespace XamlStandardPreview.Droid
+{
+    [Activity( Label = "XamlStandardPreview" , Icon = "@drawable/icon" , 
+        Theme = "@style/MainTheme" , MainLauncher = true , 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate( Bundle bundle )
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate( bundle );
+
+            global::Xamarin.Forms.Forms.Init( this , bundle );
+
+            //加入這行
+            Alias.Init();
+
+            LoadApplication( new App() );
+        }
+    }
+}
+
